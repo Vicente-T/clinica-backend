d906bbc05321a9fff9efcffd8d8edd6136d69bd6
Vicente <vicenteteixeira05@gmail>
2024-06-18 22:51:01 +0100

    feat:test6

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.use(
     }
 }))
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_TCP_PROXY_DOMAIN}:${process.env.RAILWAY_TCP_PROXY_PORT}}/${process.env.MYSQL_DATABASE}`;
 
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_TCP_PROXY_DOMAIN}:${process.env.RAILWAY_TCP_PROXY_PORT}/${process.env.MYSQL_DATABASE}`;
 const db = mysql.createConnection(urlDB);
 
 
